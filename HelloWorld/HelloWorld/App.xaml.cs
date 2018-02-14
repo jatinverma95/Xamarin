16e321c558f733855554359253922856253282c3
Jatin Verma <jatin95oct@gmail.com>
2018-02-14 20:47:17 +0530

    SimpleNavigationDone

--- HelloWorld/HelloWorld/App.xaml.cs
+++ HelloWorld/HelloWorld/App.xaml.cs
@@ -7,28 +7,32 @@ using Xamarin.Forms;
 
 namespace HelloWorld
 {
-	public partial class App : Application
-	{
-		public App ()
-		{
-			InitializeComponent();
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
 
-            MainPage = new HelloWorld.Exercises.AirbnbSearchList();
-		}
+            MainPage = new NavigationPage(new ContactsPage())
+            {
+                BarBackgroundColor = Color.Gray,
+                BarTextColor = Color.White
+            };
+        }
 
-		protected override void OnStart ()
-		{
-			// Handle when your app starts
-		}
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
 
-		protected override void OnSleep ()
-		{
-			// Handle when your app sleeps
-		}
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
 
-		protected override void OnResume ()
-		{
-			// Handle when your app resumes
-		}
-	}
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
 }
---
+++ HelloWorld/HelloWorld/NavigationnPage/Introduction.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace HelloWorld.NavigationnPage
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Introduction : ContentPage
+	{
+		public Introduction ()
+		{
+			InitializeComponent ();
+		}
+
+        async void Handle_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        protected override bool OnBackButtonPressed()
+        {
+            return true;
+        }
+    }
+}
